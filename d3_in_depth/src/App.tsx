4c6d5e5e1ac2aa149775a7cfaedb8da60db611a7
Oscar Zhang <oscarzhang228@gmail.com>
2026-02-16 20:41:28 -0600

    ocaml and d

---
+++ d3_in_depth/src/App.tsx
@@ -0,0 +1,11 @@
+import { Selections } from "./Selections";
+
+function App() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column-reverse" }}>
+      <Selections />
+    </div>
+  );
+}
+
+export default App;
---
+++ d3_in_depth/src/Selections.tsx
@@ -0,0 +1,13 @@
+export function Selections() {
+  return (
+    <svg width="760" height="140">
+      <g transform="translate(70, 70)">
+        <circle />
+        <circle cx="120" />
+        <circle cx="240" />
+        <circle cx="360" />
+        <circle cx="480" />
+      </g>
+    </svg>
+  );
+}
